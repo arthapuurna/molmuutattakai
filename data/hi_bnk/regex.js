4d16c231080422d0e56645a50c51a9c28388cb70
arthapuurna <benakasemmemii.creator@gmail.com>
2026-02-21 18:15:01 +0100

    Create regex.js

---
+++ data/hi_bnk/regex.js
@@ -0,0 +1,8 @@
+export const words = {
+  "मैं/g": "men",
+  "हिंदी/g": "Hindii-ong",
+  "बोलता हूँ/g": "molre"
+  "।": "."
+};
+export const phrases = {};
+export const orderingMap = {}; // nonbekarda
